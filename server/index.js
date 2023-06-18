4f8f56b48536e9eb9e347de0887a7c5fdccf1a21
Roman4iks <romathebestprogramist@gmail.com>
2023-06-18 17:20:23 +0300

    Add dotenv to server project

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
@@ -7,7 +8,9 @@ import { handleValidErrors, checkAuth } from './utils/index.js';
 import cors from 'cors';
 
 mongoose
-  .connect('mongodb+srv://admin:123@cluster0.fh4uljg.mongodb.net/blog?retryWrites=true&w=majority')
+  .connect(
+    `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0.fh4uljg.mongodb.net/blog?retryWrites=true&w=majority`,
+  )
   .then(() => {
     console.log('DB OK');
   })
@@ -31,10 +34,6 @@ app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
-app.get('/', async (req, res) => {
-  res.json({ message: 'hello' });
-});
-
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   res.json({
     url: `uploads/${req.file.originalname}`,
@@ -52,7 +51,7 @@ app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.postDelete);
 app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
 
-app.listen(4444, err => {
+app.listen(process.env.PORT, err => {
   if (err) {
     console.log(err);
   } else {
